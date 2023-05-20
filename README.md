1f83d09efd2ddfc533044361d7935ad5b42935e9
chintan <chintan1612199@gmail.com>
2023-05-20 10:11:25 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# React_Netflix_Movie
+
+# we make netflix clone web app in react js
+
+
+run project 
+# npm install 
+
+# npm start
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import "./home.scss";
+import List from "../../components/list/List";
+import Login from "../login/Login";
+import Register from "../register/Register";
+
+const Home = () => {
+  return (
+    <div className="home">
+      {/* <Login/> */}
+      <Register/>
+      {/* <Navbar /> */}
+      {/* <Featured/> */}
+      {/* <List/> */}
+      {/* <List/> */}
+      {/* <List/> */}
+      {/* <List/> */}
+    </div>
+  );
+};
+
+export default Home;
