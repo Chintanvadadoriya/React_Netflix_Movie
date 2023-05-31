ae0db88a536929528c533ce79bb0c292a5c1ccd3
chintan <chintan1612199@gmail.com>
2023-05-31 07:26:40 +0530

    add loader

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,12 +5,15 @@ import { AuthContext } from "../../context/authContext/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
-  const {dispatch}=useContext(AuthContext)
+  const {dispatch,user}=useContext(AuthContext)
+  const [loader,setLoader]=useState(false)
   const navigate =useNavigate()
 
   const handleLogin=(e)=>{
+    setLoader(true)
     e.preventDefault()
     login({email,password},dispatch)
   }
@@ -33,7 +36,7 @@ export default function Login() {
           <h1>Sign In</h1>
           <input type="email" placeholder="Email or phone number" onChange={(e)=>setEmail(e.target.value)}/>
           <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
-          <button className="loginButton" onClick={handleLogin}>Sign In</button>
+          <button className="loginButton" onClick={handleLogin}>{loader? "Loading...":"Sign in"}</button>
           <span  onClick={handleRegister}>
             New to Netflix? <b>Sign up now.</b>
           </span>
